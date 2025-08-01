9d0ad201a7a5d602b7c0d42a7a10c9bf7b8d88c4
SergioFraga <sergiozfraga@gmail.com>
2025-07-31 18:01:52 -0700

    Updated LibroService with .env

--- src/service/LibroService.jsx
+++ src/service/LibroService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL_LIBRO_API = "https://crud-libros-iapg.onrender.com/api/v1/libros";
+const BASE_URL_LIBRO_API = import.meta.env.VITE_API_URL + "/api/v1/libros";
 
 class LibroService {
   registrarLibro(libro) {
